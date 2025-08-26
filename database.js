9f9ecd2c4d0dd66d57358e344bb688fcd569a203
venomkage <ceomindset3@gmail.com>
2025-08-26 09:25:45 +0200

    we can create, list all and list particular for now

--- database.js
+++ database.js
@@ -1,18 +1,35 @@
 import mysql from 'mysql2';
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 const pool = mysql.createPool({
-    host:'127.0.0.1',
-    user: 'root',
-    password: '',
-    database: 'notes_app'
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
 }).promise();
 
-export const getRows = async () => {
+export const getNotes = async () => {
     // using destructuring below, it gets first element of the array
     const [rows] = await pool.query("select * from notes");
     return rows;
 }
 
+export const getNotesById = async(id) => {
+    const [row] = await pool.query(`
+            Select * from notes
+            where id = ?
+        `,[id])
+    return row[0]
+}
+
+export const createNotes = async(title, content) => {
+    const result = await pool.query(`
+            Insert into notes (title, content) values (?, ?)
+        `, [title, content]);
+    return result;
+}
 
 
 
--- server.js
+++ server.js
@@ -1,21 +1,38 @@
 import e from "express";
-import { getRows } from "./database.js";
+import { createNotes, getNotes, getNotesById } from "./database.js";
+import dotenv from "dotenv";
 
 const app = e();
+app.use(e.json());
+dotenv.config();
 
 app.get('/', (req, res) => {
     res.send("<h1>This is heading one from backend</h1>")
 })
 
-app.post('/login', (req, res) => {
-    res.send("You have reached login endpoint");
-})
-
 app.get('/notes', async (req, res) => {
-    const notes = await getRows();
+    const notes = await getNotes();
     res.send(notes)
 })
 
-app.listen(8080, () => {
-  console.log('Server is running on port 3000');
+app.get('/notes/:id', async (req, res) => {
+    const id = req.params.id;
+    const notebyid = await getNotesById(id);
+    res.send(notebyid)
+
+})
+
+app.post('/createNote', async (req, res) => {
+    const { title, content } = req.body;
+    const result = await createNotes(title, content);
+    res.send(result)
+})
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Server is running on port ${process.env.PORT || 8080}`);
 });
